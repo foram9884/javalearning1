4e0552766cce0c4106a27a2087ed479dfc44f7eb
Foram Patel <fragrance9884@gmail.com>
2022-07-28 21:18:13 +0100

    positiveNegative

---
+++ PositiveNegative.java
@@ -0,0 +1,11 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        int number=-10;
+        if (number < 0) {
+            System.out.println("Number is negative");
+        }else{
+            System.out.println("Number is positive");
+
+        }
+    }
+}
